3cc81859bc6d6fe91b13c4c740b49312b9bcc590
gerarjon <gerarsuaverdez@gmail.com>
2019-08-13 21:40:28 -0700

    added character object and some css visualization

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,39 @@
 $(document).ready(function(){
     
+    // Characters //
+    var characters = {
+      "Puffy Pomeranian": {
+        name: "Puffy Pomeranian",
+        health: 200,
+        attack: 5,
+        imageUrl: "assets/images/pom-1.png",
+        enemyAttack: 15,
+      },
+
+      "Lazy Labrador": {
+        name: "Lazy Labrador",
+        health: 100,
+        attack: 21,
+        imageUrl: "assets/images/lab-1.png",
+        enemyAttack: 9,
+      },
+
+      "Busy Boxer": {
+        name: "Busy Boxer",
+        health: 150,
+        attack: 10,
+        imageUrl: "assets/images/boxer-1.png",
+        enemyAttack: 8,
+      },
+
+      "Sneaky Shiba": {
+        name: "Sneaky Shiba",
+        health:  69,
+        attack: 69,
+        imageUrl: "assets/images/shiba-1.png",
+        enemyAttack: 7,
+      },
+    };
+
+    console.log(characters);
   });
