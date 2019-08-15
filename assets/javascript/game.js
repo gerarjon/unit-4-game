0089e2ad2eaf4a115a1b4e44114fc1649c67a042
gerarjon <gerarsuaverdez@gmail.com>
2019-08-15 16:36:15 -0700

    changed the code of everything and fixed the sizes of images

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
     
     // Characters //
+    //-----------------------------------------------
     var characters = {
       "Puffy Pomeranian": {
         name: "Puffy Pomeranian",
@@ -35,5 +36,45 @@ $(document).ready(function(){
       },
     };
 
-    console.log(characters);
+    // Selected character
+    var fighter;
+    // Opponents to select
+    var opponent;
+    // Chosen opponent 
+    var chosenOpponent;
+    // Tracks numbner of turns
+    var turnCounter
+    // Tracks number of opponents defeated
+    var opponentsDefeated;
+
+    // Game object
+    //--------------------------------------------------
+    var game = {
+      //Functions
+      //------------------------------------------------
+
+      // This function displays characters to the page 
+      displayChar: function(character, displayArea) {
+        var charDiv = $("<div class='character' data-name='" + character.name + "'>");
+        var charName = $("<div class='character-name'>").text(character.name);
+        var charImage = $("<img alt='image' class='character-image'>").attr("src", character.imageUrl);
+        var charHealth = $("<div class='character-health'>").text("Health: " + character.health);
+        charDiv.append(charName).append(charImage).append(charHealth);
+        $(displayArea).append(charDiv);
+      },
+
+      // function that loops through chacters in characters object
+      startGame: function() {
+        for ( var key in characters ) {
+          this.displayChar(characters[key], "#character-selection");
+        }
+      }
+    };
+
+    // Running startGame()
+    game.startGame();
+
+    
+
+
   });
